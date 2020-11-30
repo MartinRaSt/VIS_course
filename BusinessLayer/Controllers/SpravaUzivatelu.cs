70a8237c40db06bc1be39b9cd4116d91c94e76ce
MartinRaSt <radvansky@radvansky.net>
2020-12-01 00:45:35 +0100

    Rozpracován Gateway pro Uživatele se zápisem do DB

---
+++ BusinessLayer/Controllers/SpravaUzivatelu.cs
@@ -0,0 +1,147 @@
+﻿#region FileDescription
+
+// **************************************************************************************************
+// Projekt: BusinessLayer - SpravaUzivatelu.cs
+// Created:  01.12.2020 0:18
+// Modified: 01.12.2020 2020
+// Description: Správa uživatelů v IS
+// ***************************************************************************************************
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using BusinessLayer.BO;
+using BusinessLayer.Enums;
+using DataLayer.TableDataGateways;
+
+namespace BusinessLayer.Controllers
+{
+    public class SpravaUzivatelu
+    {
+        #region Privátní proměnné
+
+        private static readonly object m_LockObj = new object();
+        private static SpravaUzivatelu m_Instance;
+
+        private List<Uzivatel> m_Uzivatele;
+        #endregion
+
+        #region Veřejné proměnné
+
+        public static SpravaUzivatelu Instance
+        {
+            get
+            {
+                lock (m_LockObj)
+                {
+                    return m_Instance ?? (m_Instance = new SpravaUzivatelu());
+                }
+            }
+        }
+
+        /// <summary>
+        /// Seznam všech uživatelů
+        /// </summary>
+        public List<Uzivatel> Uzivatele
+        {
+            get => m_Uzivatele;
+        }
+
+        /// <summary>
+        /// Celkový počet Uzivatelů
+        /// </summary>
+        public int CelkovyPocetUzivatelu => m_Uzivatele.Count;
+
+        #endregion
+
+        #region Privátní metody
+
+        /// <summary>
+        /// Objekt spravy uživatelů
+        /// </summary>
+        private SpravaUzivatelu()
+        {
+            m_Uzivatele = new List<Uzivatel>();
+            NacteniZUloziste();
+        }
+
+        private bool NacteniZUloziste()
+        {
+            DataTable dtUzivatele = null;
+            string errMsg = string.Empty;
+            if (UzivateleGW.Instance.LoadAll(out dtUzivatele, out errMsg))
+            {
+                if (dtUzivatele != null)
+                {
+                    foreach (DataRow row in dtUzivatele.Rows)
+                    {
+                        Uzivatele.Add(new Uzivatel()
+                        {
+                            Id = (long)row["Id"],
+                            Jmeno = row["Jmeno"].ToString(),
+                            Prijmeni = row["Prijmeni"].ToString(),
+                            DatumNarozeni = (DateTime)row["DatumNarozeni"],
+                            ClenemOd = (DateTime)row["ClenemOd"],
+                            Spolehlivost = (EnHodnoceni)((int)row["Spolehlivost"])
+                        });
+                    }
+                }
+                return true;
+            }
+            else
+            {
+                //Zalogujeme někam chybu
+                throw new Exception($"Uživatelé: Načteni uživatelů z uložiště \n{errMsg}");
+            }
+        }//NacteniZUloziste
+
+        private bool ZapisDoUloziste()
+        {
+            string errMsg = string.Empty;
+
+            //Pomocny seznam pro ukladani
+            DataTable dtUzivatele = new DataTable();
+
+            //Naplneni uzivatelu do datatable
+            foreach (var zam in m_Uzivatele)
+            {
+                //lstZam.Add(
+                //    new ZamestnanecStruct(zam.Id == -1 ? maxID++ : zam.Id,
+                //        zam.Jmeno, zam.Prijmeni, zam.ZamestnanOd, (uint)zam.TypZamestnance));
+            }
+
+            if (!UzivateleGW.Instance.SaveAll(dtUzivatele, out errMsg))
+            {
+                //Zalogujeme někam chybu
+                throw new Exception($"Uzivatele: ZapisDoUloziste \n{errMsg}");
+            }
+            return true;
+        }//ZapisDoUloziste
+
+        #endregion
+
+        #region Veřejné metody
+
+        /// <summary>
+        /// Zapsani vsech uzivatelu do uloziste
+        /// </summary>
+        /// <returns></returns>
+        public bool SaveAll()
+        {
+            return ZapisDoUloziste();
+        }
+
+        /// <summary>
+        /// Vložení nového zaměstnance
+        /// </summary>
+        /// <param name="uzivatel"> Objekt třídy uzivatel</param>
+        public void AddZamestnanec(Uzivatel uzivatel)
+        {
+            m_Uzivatele.Add(uzivatel);
+        }
+
+        #endregion
+    }//class
+}//namespace
--- DataLayer/TableDataGateways/UzivateleGW.cs
+++ DataLayer/TableDataGateways/UzivateleGW.cs
@@ -10,6 +10,7 @@
 #endregion
 
 using System;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace DataLayer.TableDataGateways
@@ -53,45 +54,285 @@ namespace DataLayer.TableDataGateways
         #endregion
 
         #region Veřejné metody
+        /// <summary>
+        /// Načtení všech uživatelů z databáze do objektu typu DataTable (DTO)
+        /// </summary>
+        /// <param name="dtUzivatele">Tabulka načtených uživatelů</param>
+        /// <param name="errMsg">Chybové hlášení, pokud nastala chyba</param>
+        /// <returns>True - načtení proběhlo bez chyby, False - chyba při načítání</returns>
+        public bool LoadAll(out DataTable dtUzivatele, out string errMsg)
+        {
+            dtUzivatele = null;
+            errMsg = string.Empty;
 
-        public bool LoadAll(out string errMsg)
+            var sql = "SELECT Id, Jmeno, Prijmeni, DatumNarozeni, ClenemOd, Spolehlivost FROM Uzivatele";
+
+            //Vlozeni nebo aktualizace uzivatele v ulozisti
+            try
+            {
+                DataConnection.Instance.Connect();
+                try
+                {
+                    SqlCommand sqlCmd = DataConnection.Instance.CreateCommand(sql);
+                    try
+                    {
+                        try
+                        {
+                            var result = DataConnection.Instance.Select(sqlCmd);
+                            dtUzivatele = new DataTable();
+                            dtUzivatele.Load(result);
+                        }
+                        catch (Exception e)
+                        {
+                            //nastala chyba vykonání SELECT
+                            dtUzivatele = null;
+                            errMsg = $"Chyba při SELECT tabulky Uživatelů \n{e.Message}";
+                            return false;
+                        }
+                    }
+                    finally
+                    {
+                        sqlCmd.Dispose();
+                    }
+                }
+                finally
+                {
+                    DataConnection.Instance.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                errMsg = $"Chyba při Connection do DB \n{e.Message}";
+                return false;
+            }
+            return true;
+        }//LoadAll
+
+        /// <summary>
+        /// Uložení všech objektů uživatelů do Databáze provede jejich Insert/Update a to v DB transakci
+        /// </summary>
+        /// <param name="dtUzivatele">DataTable se seznamem uživatelů, které chcecem uložit do DB</param>
+        /// <param name="errMsg">Chybové hlášení pokud nastala chyba</param>
+        /// <returns>True - operace se provedla, False - nastala chyba</returns>
+        public bool SaveAll(DataTable dtUzivatele, out string errMsg)
         {
             errMsg = string.Empty;
 
+            string sqlInsert =
+                "INSERT INTO Uzivatele (Jmeno,Prijmeni,DatumNarozeni,ClenemOd,Spolehlivost) " +
+                "VALUES (@jm,@prij,@datN,@clenO,@spol); SELECT SCOPE_IDENTITY();";
+            string sqlUpdate =
+                "UPDATE Uzivatele SET Jmeno = @jm, Prijmeni = @prij, DatumNarozeni = @datN, " +
+                "ClenemOd = @clenO, Spolehlivost = @spol WHERE (Id=@id)";
+
+            long id = -1;
+            string jmeno = string.Empty;
+            string prijmeni = String.Empty;
+            DateTime datumNarozeni = DateTime.MinValue;
+            DateTime clenemOd = DateTime.MinValue;
+            UInt16 spolehlivost = 0;
+
+            //Vlozeni nebo aktualizace uzivatele v ulozisti
+            try
+            {
+                DataConnection.Instance.Connect();
+                try
+                {
+                    DataConnection.Instance.BeginTransaction();
+                    for (int i = 0; i < dtUzivatele.Rows.Count; i++)
+                    {
+
+                        id = (long) dtUzivatele.Rows[i]["Id"];
+                        jmeno = dtUzivatele.Rows[i]["Jmeno"].ToString();
+                        prijmeni = dtUzivatele.Rows[i]["Prijmeni"].ToString();
+                        datumNarozeni = (DateTime) dtUzivatele.Rows[i]["DatumNarozeni"];
+                        clenemOd = (DateTime) dtUzivatele.Rows[i]["ClenemOd"];
+                        spolehlivost = (UInt16) dtUzivatele.Rows[i]["Spolehlivost"];
+
+                        var sql = id < 0 ? sqlInsert : sqlUpdate;
+                        SqlCommand sqlCmd = DataConnection.Instance.CreateCommand(sql);
+                        try
+                        {
+                            sqlCmd.Parameters.AddWithValue("@id", id);
+                            sqlCmd.Parameters.AddWithValue("@jm", jmeno);
+                            sqlCmd.Parameters.AddWithValue("@prij", prijmeni);
+                            sqlCmd.Parameters.AddWithValue("@datN", datumNarozeni);
+                            sqlCmd.Parameters.AddWithValue("@clenO", clenemOd);
+                            sqlCmd.Parameters.AddWithValue("@spol", spolehlivost);
+                            try
+                            {
+                                var result = DataConnection.Instance.ExecuteNonQuery(sqlCmd);
+                                //Pokud je návratová hodnota záporná nepovedlo se vložit/upravit
+                                if (result < 0)
+                                    throw new DataException($"Nepovedlo se uložit Uživatele ID:({id})");
+                            }
+                            catch (Exception e)
+                            {
+                                //nastala chyba vykonání INSERT/UPDATE - vrátíme změny v DB
+                                DataConnection.Instance.Rollback();
+                                errMsg = $"Chyba při ukládání objektů Uživatelů \n{e.Message}";
+                                return false;
+                            }
+                        }
+                        finally
+                        {
+                            sqlCmd.Dispose();
+                        }
+                    }
+                    DataConnection.Instance.EndTransaction();
+                }
+                finally
+                {
+                    DataConnection.Instance.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                errMsg = $"Chyba při Connection do DB \n{e.Message}";
+                return false;
+            }
             return true;
-        }
+        }//SaveAll
 
-        public bool SaveAll(out string errMsg)
-        {
-            errMsg = string.Empty;
 
-            return true;
-        }
-
+        /// <summary>
+        /// Vložení nebo aktualizace Uživatele v DB
+        /// </summary>
+        /// <param name="id">Pokud je záporné ID, pak se jedná o nového uživatele a bude vytvořen, id po návratu obsahujé přidělené ID z DB, jinak se provede update</param>
+        /// <param name="jmeno">Jméno uživatele</param>
+        /// <param name="prijmeni">Příjmení</param>
+        /// <param name="datumNarozeni">Datum narození</param>
+        /// <param name="clenemOd">Datum, od kterého je uživatel členem knihovny</param>
+        /// <param name="spolehlivost">Typ spolehlivosti uživatele</param>
+        /// <param name="errMsg">Chybové hlášení pokud nastala chyba</param>
+        /// <returns>True nebyla chyba, False chyba nastala</returns>
         public bool InsertOrUpdate(ref long id, 
-            out string jmeno, 
-            out string prijmeni,
-            out DateTime datumNarozeni,
-            out DateTime clenemOd,
-            out UInt16 spolehlivost,
+            string jmeno, 
+            string prijmeni,
+            DateTime datumNarozeni,
+            DateTime clenemOd,
+            UInt16 spolehlivost,
             out string errMsg)
         {
             errMsg = string.Empty;
-            jmeno = string.Empty;
-            prijmeni = string.Empty;
-            datumNarozeni = DateTime.MinValue;
-            clenemOd = DateTime.MinValue;
-            spolehlivost = 0;
 
+            string sqlInsert =
+                "INSERT INTO Uzivatele (Jmeno,Prijmeni,DatumNarozeni,ClenemOd,Spolehlivost) " +
+                "VALUES (@jm,@prij,@datN,@clenO,@spol); SELECT SCOPE_IDENTITY();";
+            string sqlUpdate =
+                "UPDATE Uzivatele SET Jmeno = @jm, Prijmeni = @prij, DatumNarozeni = @datN, " +
+                "ClenemOd = @clenO, Spolehlivost = @spol WHERE (Id=@id)";
+            var sql = id < 0 ? sqlInsert : sqlUpdate;
+
+            //Vlozeni nebo aktualizace uzivatele v ulozisti
+            try
+            {
+                DataConnection.Instance.Connect();
+                try
+                {
+                    DataConnection.Instance.BeginTransaction();
+                    SqlCommand sqlCmd = DataConnection.Instance.CreateCommand(sql);
+                    try
+                    {
+                        sqlCmd.Parameters.AddWithValue("@id", id);
+                        sqlCmd.Parameters.AddWithValue("@jm", jmeno);
+                        sqlCmd.Parameters.AddWithValue("@prij", prijmeni);
+                        sqlCmd.Parameters.AddWithValue("@datN", datumNarozeni);
+                        sqlCmd.Parameters.AddWithValue("@clenO", clenemOd);
+                        sqlCmd.Parameters.AddWithValue("@spol", spolehlivost);
+                        try
+                        {
+                            var result = DataConnection.Instance.ExecuteNonQuery(sqlCmd);
+                            //Pokud je návratová hodnota záporná nepovedlo se vložit/upravit
+                            if (result < 0)
+                                throw new DataException($"Nepovedlo se vložit/upravit Uživatele ID:({id})");
+                            DataConnection.Instance.EndTransaction();
+                            if (id < 0)
+                                id = result;  //id vytvořené na DB serveru a přidělené novému záznamu v DB
+                        }
+                        catch (Exception e)
+                        {
+                            //nastala chyba vykonání INSERT/UPDATE - vrátíme změny v DB
+                            DataConnection.Instance.Rollback();
+                            errMsg = $"Chyba při INSERT/UPDATE objektu Uživatele \n{e.Message}";
+                            return false;
+                        }
+                    }
+                    finally
+                    {
+                        sqlCmd.Dispose();
+                    }
+                }
+                finally
+                {
+                    DataConnection.Instance.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                errMsg = $"Chyba při Connection do DB \n{e.Message}";
+                return false;
+            }
             return true;
-        }
+        }//InsertOrUpdate
 
+        /// <summary>
+        /// Smazání uživatele z DB uložiště
+        /// </summary>
+        /// <param name="id">ID uživatele</param>
+        /// <param name="errMsg">Chybové hlášení</param>
+        /// <returns>True smazání se povedlo, False nepovedlo</returns>
         public bool Delete(long id, out string errMsg)
         {
             errMsg = string.Empty;
 
+            var sql = "DELETE FROM Uzivatele WHERE (Id = @id)";
+
+            //Vlozeni nebo aktualizace uzivatele v ulozisti
+            try
+            {
+                DataConnection.Instance.Connect();
+                try
+                {
+                    DataConnection.Instance.BeginTransaction();
+                    SqlCommand sqlCmd = DataConnection.Instance.CreateCommand(sql);
+                    try
+                    {
+                        sqlCmd.Parameters.AddWithValue("@id", id);
+                        try
+                        {
+                            var result = DataConnection.Instance.ExecuteNonQuery(sqlCmd);
+                            //Pokud je návratová hodnota záporná nepodlo se smazat uživatele
+                            if (result < 0)
+                                throw new DataException($"Nepovedlo se smazat Uživatele ID:({id})");
+
+                            DataConnection.Instance.EndTransaction();
+                        }
+                        catch (Exception e)
+                        {
+                            //nastala chyba vykonání DELETE - vrátíme změny v DB
+                            DataConnection.Instance.Rollback();
+                            errMsg = $"Chyba při DELETE objektu Uživatele \n{e.Message}";
+                            return false;
+                        }
+                    }
+                    finally
+                    {
+                        sqlCmd.Dispose();
+                    }
+                }
+                finally
+                {
+                    DataConnection.Instance.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                errMsg = $"Chyba při Connection do DB \n{e.Message}";
+                return false;
+            }
             return true;
-        }
+        }//Delete
 
         /// <summary>
         /// Nalezeni uzivatele na zaklade jeho ID
@@ -163,12 +404,12 @@ namespace DataLayer.TableDataGateways
             }
             catch (Exception e)
             {
-                errMsg = e.Message;
+                errMsg = $"Chyba při Connection do DB \n{e.Message}";
                 return false;
             }
 
             return true;
-        }
+        }//Find
         #endregion
 
     }//class
